39f51f93c8a0a0aeeb6533c72de7cddcc3f127fa
Racheal Orukele <orukele_racheal@yahoo.com>
2022-11-30 17:25:17 +0100

    add alt text

--- src/Post.js
+++ src/Post.js
@@ -6,7 +6,7 @@ function Post() {
   const [document] = useSinglePrismicDocument("post");
   console.log("post", document);
 
-  // console.log("here", document)  document.data.title
+  // console.log("here", document)  alt={document.data.profile[0].profile_image.alt}
 
   return (
     <>
@@ -14,7 +14,10 @@ function Post() {
         <div>
           <PrismicRichText field={document.data.page_title} />
           <PrismicRichText field={document.data.post_text} />
-          <img src={document.data.post_image.url} />
+          <img
+            src={document.data.post_image.url}
+            alt={document.data.post_image.alt}
+          />
         </div>
       )}
 
@@ -22,7 +25,10 @@ function Post() {
         <div>
           <PrismicRichText field={document.data.profile[0].profile_heading} />
           <PrismicRichText field={document.data.profile[0].profile_body} />
-          <img src={document.data.profile[0].profile_image.url} />
+          <img
+            src={document.data.profile[0].profile_image.url}
+            alt={document.data.profile[0].profile_image.alt}
+          />
           <p>{document.data.profile[0].first_publication_date}</p>
         </div>
       )}
