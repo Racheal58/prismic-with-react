ce01156dc722bb36e966396d293fb5d633c43c4c
Racheal Orukele <orukele_racheal@yahoo.com>
2022-12-20 02:24:38 +0100

    change class to className

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div class="flex justify-center mt-10">
-      <div class="w-7/12 flex justify-end">@racheal 20210</div>
+    <div className="flex justify-center mt-10">
+      <div className="w-7/12 flex justify-end">@racheal 20210</div>
     </div>
   );
 };
